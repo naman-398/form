96f7d3ecf45ef7ebed0652aae241699f3eb82c9c
naman-398 <you@example.com>
2024-08-07 12:59:50 +0530

    done

--- src/components/common/InputField.jsx
+++ src/components/common/InputField.jsx
@@ -19,7 +19,7 @@ const InputField = ({
           value={value}
           onChange={onChange}
           required={required}
-          className="placeholder:font-normal pr-4 border-none outline-none placeholder:text-xs lg:placeholder:text-sm placeholder:text-white text-white font-normal text-xs lg:text-sm w-full !p-3 sm:!p-[15.5px_16px_19px_16px] rounded-lg bg-black-olive"
+          className="placeholder:font-normal pr-4 border-none outline-none min-h-10 placeholder:text-xs lg:placeholder:text-sm placeholder:text-white text-white font-normal text-xs lg:text-sm w-full !p-3 sm:!p-[15.5px_16px_19px_16px] rounded-lg bg-black-olive"
         >
           <option value="" className='!p-3 sm:!p-[15.5px_16px_19px_16px]'>{label}</option>
           {options.map((option) => (
