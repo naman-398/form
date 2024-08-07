ed1a8f6d18b92d52c02f6a47735d29cd435223ee
naman-398 <you@example.com>
2024-08-07 12:51:17 +0530

    done

---
+++ src/components/common/InputField.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const InputField = ({
+  type = 'text',
+  name,
+  value,
+  onChange,
+  label,
+  placeholder,
+  required = false,
+  textarea = false,
+  options = [],
+}) => {
+  return (
+    <>
+      {type === 'select' ? (
+        <select
+          name={name}
+          value={value}
+          onChange={onChange}
+          required={required}
+          className="placeholder:font-normal pr-4 border-none outline-none placeholder:text-xs lg:placeholder:text-sm placeholder:text-white text-white font-normal text-xs lg:text-sm w-full p-3 sm:p-[15.5px_16px_19px_16px] rounded-lg bg-black-olive"
+        >
+          <option value="">{label}</option>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          required={required}
+          className="placeholder:font-normal pr-4 border-none outline-none placeholder:text-xs lg:placeholder:text-sm placeholder:text-white text-white font-normal text-xs lg:text-sm w-full p-3 sm:p-[15.5px_16px_19px_16px] rounded-lg bg-black-olive"
+        />
+      )}
+    </>
+  );
+};
+
+export default InputField;
